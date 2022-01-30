7dfbab0fa922c7b82ce4d3ab808a6e4104fc2123
Andrii Berezhko <Berezhko7777@ukr.net>
2022-01-31 00:35:35 +0200

    bug-fix
    исправил все что смог))

--- src/mobilemenu.js
+++ src/mobilemenu.js
@@ -3,18 +3,26 @@
       openMenuBtn: document.querySelector('[data-menu-open]'),
       closeMenuBtn: document.querySelector('[data-menu-close]'),
       menu: document.querySelector('[data-menu]'),
-    //   menu: document.querySelector('[menu-open]'),
+    
       
     };
   
     refs.openMenuBtn.addEventListener('click', toggleMenu);
-    refs.closeMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click',toggleMenu);
   
   function toggleMenu() {
       document.body.classList.toggle('menu-open')
       refs.menu.classList.toggle('is-hidden');
-    //   refs.menu.classList.toggle('is-open');
       document.querySelector('.hero').classList.toggle("menu-is-open");
     }
-  })();
+})();
+  
+;(function (){
+  const nav = document.querySelector(".nav__list");
+  nav.addEventListener("click", function(ev){
+    document.querySelector('.mobile__menu').classList.add("is-hidden");
+    document.body.classList.remove('menu-open')
+    document.querySelector('.backdrop').classList.toggle("menu-is-close");
+  });
+})();
 
